ffd4dcd6ea85d860ba093e79c1ead8271600a0c0
AlbinIzi <albin.carpentier@iziwork.com>
2020-04-03 16:09:30 +0200

    do not use absolute link

--- src/consent-manager/banner.tsx
+++ src/consent-manager/banner.tsx
@@ -74,7 +74,7 @@ export default class Banner extends PureComponent<Props> {
             paramétrer en <button type="button" onClick={onChangePreferences}>cliquant ici</button>.
           </P>
           <P>
-            [<a href="https://www.iziwork.com/cookies" target="_blank" rel="noopener noreferrer">
+            [<a href="/cookies" target="_blank" rel="noopener noreferrer">
               Plus d’informations
             </a>] | [<CloseButton type="button" onClick={onClose}>
               Accepter
