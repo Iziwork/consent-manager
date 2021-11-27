79cc5b36c30ab193e235809c22f3b8f6ca9f2eff
Albin Carpentier <albin.carpentier@iziwork.com>
2021-11-27 10:03:36 +0100

    remove incompatible module

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,39 +5,23 @@ module.exports = {
   mode: 'development',
   devtool: 'source-map',
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.stories\.tsx?$/,
-        loaders: [
-          {
-            loader: require.resolve('@storybook/source-loader'),
-            options: {
-              parser: 'typescript',
-              prettierConfig: {
-                printWidth: 100,
-                singleQuote: false
-              }
-            }
-          }
-        ],
-        enforce: 'pre'
-      },
-      {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: 'ts-loader'
-      }
-    ]
+        loader: 'ts-loader',
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
-        VERSION: JSON.stringify(pkg.version)
-      }
-    })
-  ]
+        VERSION: JSON.stringify(pkg.version),
+      },
+    }),
+  ],
 }
